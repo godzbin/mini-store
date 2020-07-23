a1578a9c2519adb9c9a73e4110ee3647f387bfad
chenzebin <qq675402455@163.com>
2020-07-23 18:19:58 +0800

    换一个目录

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/app', indexRouter);
 app.use('/users', usersRouter);
--- store-web/vue.config.js
+++ store-web/vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
   // 基本路径
   publicPath: './',
   // 打包文件目录
-  outputDir: '../public',
+  outputDir: '../dist',
   // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
   assetsDir: '',
   // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径
